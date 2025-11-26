ddd86f85182419fa9c5ed8f98d4d26517a710564
martin <martinfl.dev@gmail.com>
2025-11-26 17:21:14 -0300

    feat: add docker compose

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,7 +7,7 @@
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 dotenv.config({
-  path: path.resolve(__dirname, '../.env'),
+  path: path.resolve(__dirname, '../../../.env'),
   override: true,
 });
 
